f5493d999e9420f8ad4686054b92f7e261ae87ba
Enrique Garrido Pozo <enrique.garrido@alu.uclm.es>
2019-01-29 17:15:46 +0100

    FPLClasses Done

---
+++ TySWeb_Practica/src/edu/uclm/esi/tysweb/games/fpl/FPLBoard.java
@@ -0,0 +1,5 @@
+package edu.uclm.esi.tysweb.games.fpl;
+
+public class FPLBoard {
+
+}
---
+++ TySWeb_Practica/src/edu/uclm/esi/tysweb/games/fpl/FPLMatch.java
@@ -0,0 +1,28 @@
+package edu.uclm.esi.tysweb.games.fpl;
+
+import edu.uclm.esi.tysweb.games.Match;
+import edu.uclm.esi.tysweb.games.Player;
+import edu.uclm.esi.tysweb.games.Result;
+import edu.uclm.esi.tysweb.games.rps.RPSBoard;
+
+public class FPLMatch extends Match{
+
+	@Override
+	protected void save() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void calculateFirstPlayer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean tieneElTurno(Player player) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
