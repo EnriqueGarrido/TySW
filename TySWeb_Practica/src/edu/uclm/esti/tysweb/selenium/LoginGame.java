52c0fb8f8a9311ffd3ddbce6b942b670bef8dd9e
Enrique Garrido Pozo <enrique.garrido@alu.uclm.es>
2019-02-01 18:09:38 +0100

    SELENIUM ---> New dependencies // Navegator // Login

---
+++ TySWeb_Practica/src/edu/uclm/esti/tysweb/selenium/LoginGame.java
@@ -0,0 +1,83 @@
+package edu.uclm.esti.tysweb.selenium;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.Select;
+
+public class LoginGame {
+  private WebDriver driver;
+ // private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+	System.setProperty("webdriver.chrome.driver", "D:\\Github\\TySWeb_Practica\\TySW\\Navegador\\chromedriver.exe");
+	driver = new ChromeDriver();
+    //driver = new FirefoxDriver();
+    //baseUrl = "https://www.katalon.com/";
+    //driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testLoginGame() throws Exception {
+    driver.get("http://localhost:8080/TySWeb_Practica/index.html");
+    driver.findElement(By.id("email")).click();
+    driver.findElement(By.id("email")).clear();
+    driver.findElement(By.id("email")).sendKeys("ana@nita.com");
+    driver.findElement(By.id("email")).sendKeys(Keys.ENTER);
+    driver.findElement(By.id("pass")).click();
+    driver.findElement(By.id("pass")).clear();
+    driver.findElement(By.id("pass")).sendKeys("ana123");
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='create'])[1]/following::button[1]")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
