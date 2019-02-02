5ab11dc1031134e725883cf724c3e0a7f0612342
Enrique Garrido Pozo <enrique.garrido@alu.uclm.es>
2019-02-02 13:31:18 +0100

    Route of the navigators are bad

--- TySWeb_Practica/src/edu/uclm/esi/tysweb/selenium/FPLGame.java
+++ TySWeb_Practica/src/edu/uclm/esi/tysweb/selenium/FPLGame.java
@@ -23,10 +23,10 @@ public class FPLGame {
 
 @Before
   public void setUp() throws Exception {
-	System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+	System.setProperty("webdriver.chrome.driver", "D:\\Github\\TySWeb_Practica\\TySW\\Navegador\\chromedriver.exe");
 	driver1 = new ChromeDriver();
 	
-	System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
+	System.setProperty("webdriver.gecko.driver", "D:\\Github\\TySWeb_Practica\\TySW\\Navegador\\geckodriver.exe");
 	driver2 = new FirefoxDriver();
     driver1.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     driver2.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
